1c3bedada24cff7f747680086bef54912495135f
Jkorf <jankorf91@gmail.com>
2026-02-11 14:02:54 +0100

    Fixed missing mapping on LoanTerm enum

--- ByBit.Net/Enums/LoanTerm.cs
+++ ByBit.Net/Enums/LoanTerm.cs
@@ -1,3 +1,4 @@
+using CryptoExchange.Net.Attributes;
 using System.Text.Json.Serialization;
 
 namespace Bybit.Net.Enums
@@ -11,22 +12,27 @@ namespace Bybit.Net.Enums
         /// <summary>
         /// 7 days
         /// </summary>
+        [Map("7")]
         Days7,
         /// <summary>
         /// 14 days
         /// </summary>
+        [Map("14")]
         Days14,
         /// <summary>
         /// 30 days
         /// </summary>
+        [Map("30")]
         Days30,
         /// <summary>
         /// 90 days
         /// </summary>
+        [Map("90")]
         Days90,
         /// <summary>
         /// 180 days
         /// </summary>
+        [Map("180")]
         Days180
     }
 }
